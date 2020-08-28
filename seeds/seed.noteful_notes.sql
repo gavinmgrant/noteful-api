d7c633bb262e7fffd2f96b3863e637604b8f1eed
Gavin Grant <gavingrant@gmail.com>
2020-08-28 16:59:11 -0700

    added seeding script

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,10 @@
+INSERT INTO noteful_folders (folder_name)
+VALUES
+('Folder one');
+
+INSERT INTO noteful_notes (note_name, content, folder)
+VALUES
+('Note one', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 1),
+('Note two', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 1),
+('Note three', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 1),
+('Note four', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 1);
